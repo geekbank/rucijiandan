a481e7adac1735c0a1cd8fb3ecb38ed72fb2f8a5
liu Jie <289381862@qq.com>
2017-10-22 21:38:58 +0800

    1.16
    修改了一些css错误，删除了没写好的js

--- js/geek.js
+++ js/geek.js
@@ -9,17 +9,3 @@ $(function(){
     $('.actGotop').click(function(){
     $('html,body').animate({scrollTop: '0px'}, 800);});   
 });
-$("download").mouseover(function(){
-  $("download").css("background: url(down_btn_hover.png)");
-});
-
-$("download")(function () {
-            //弹出层
-            $("#download").click(function (event) {
-                $("download").show(300);
-            });
-            //隐藏层
-            $("download").click(function (event) {
-                $("download").hide(300);
-            });
-        });
